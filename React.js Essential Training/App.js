5929e1d2612d48c504159d51ccf55a182096cc49
Hadassah Latchague <38115851+hlatchague1@users.noreply.github.com>
2021-01-26 07:42:23 -0800

    Add files via upload

---
+++ React.js Essential Training/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import {useEffect, useState} from "react";
+
+
+function App() {
+  const [emotion, setEmotion] = useState("happy");
+  const [secondary, setSecondary] = useState("tired");
+
+  useEffect(()=>{
+    console.log(`It's ${emotion} around here!`);
+  }, [emotion]);
+
+  useEffect(()=>{
+    console.log(`It's ${secondary} around here!`)
+  }, [secondary])
+
+  return(
+    <>
+      <h1>Current emotions are {emotion} and {secondary}.</h1>
+      <button onClick={() => setEmotion("happy")}> Make Happy</button>
+      <button onClick={() => setSecondary("crabby")}>Make Crabby</button>
+      <button onClick={() => setEmotion("frustrated")}> Frustrate</button>
+      <button onClick={() => setEmotion("enthusiastic")}> Enthuse</button>
+    </>
+  )
+}
+
+export default App;
